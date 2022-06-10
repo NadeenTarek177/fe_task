6c2e4d0e29ab58c98c2cde7ca694b6ae39371335
NadeenTarek177 <nadeentarek99@gmail.com>
2022-06-10 14:24:51 +0200

    Add files

---
+++ app/components/vue-components/performance-chart.vue
@@ -0,0 +1,150 @@
+<template>
+<div>
+<p>{{start}}-->{{typeof(Date.parse(start))}}</p>
+<p>{{end}}-->{{Date.parse(end)}}</p>
+
+  <div class="c-chart__container">
+    <v-chart ref="chart" :option="chartOptions" />
+  </div>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+import { use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { LineChart } from "echarts/charts";
+import {
+  TitleComponent,
+  TooltipComponent, 
+  GridComponent,
+  VisualMapComponent,
+} from "echarts/components";
+import VChart from "vue-echarts";
+import axios from "axios";
+
+use([
+  CanvasRenderer,
+  LineChart,
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  VisualMapComponent,
+]);
+
+export default {
+  name: "PerformanceChartComponent",
+
+  components: {
+    VChart,
+  },
+
+  props: ['start' ,'end'],
+  data() {
+    return {
+      chartData: [],
+      chartPoints: [],
+    };
+  },
+
+  created() {
+        this.getchartData();
+    },
+  beforeUpdate(){
+    if(this.start && this.end){
+      this.filterData(Date.parse(this.start) , Date.parse(this.end));
+    }
+  },
+
+  computed: {
+    initOptions() {
+      return {
+        width: "auto",
+        height: "300px",
+      };
+    },
+
+    chartOptions() {
+      return {
+        title: {
+          text: "Team Performance Index",
+          left: "center",
+        },
+        tooltip: {
+          trigger: 'axis',
+          transitionDuration: 0,
+          confine: false,
+          hideDelay: 0,
+          padding: 0,
+        },
+        grid: {
+          left: "30px",
+          right: "12px",
+          bottom: "2px",
+          top: "6px",
+          containLabel: true,
+        },
+        xAxis: {
+          type: "category",
+          showGrid: false,
+          data: this.xAxisData,
+          axisLine: {
+            show: true,
+          },
+          axisTick: {
+            show: false,
+          },
+          axisLabel: {
+            fontSize: 11,
+          },
+        },
+        yAxis: {
+          axisLabel: { show: true },
+          axisTick: { show: true },
+          splitLine: { show: true },
+        },
+        series: [
+          {
+            data: this.yAxisData,
+            type: "line",
+            symbol: "circle",
+            symbolSize: 2,
+            cursor: "default",
+            lineStyle: {
+              width: 2,
+            },
+          },
+        ],
+      };
+    },
+
+    xAxisData() {
+      return this.chartPoints.map((item) => this.formatDate(item.date_ms));
+    },
+
+    yAxisData() {
+      return this.chartPoints.map((item) => +item.performance * 100);
+    },
+  },
+
+  methods: {
+    getchartData() {
+      axios.get("https://fe-task.getsandbox.com/performance").then((Response) => {
+        this.chartData = Response.data;
+        this.chartPoints = Response.data;
+      }).catch((error)=>console.log(error))
+    },
+    formatDate(dateInMs) {
+      return moment(dateInMs).format("DD MMM YYYY");
+    },
+    filterData(startdate,enddate){
+      if(this.chartData[this.chartData.length-1].date_ms>=enddate)
+      this.chartPoints = this.chartData.filter((data) => (data.date_ms >= startdate && data.date_ms <= enddate));
+   else
+   { alert("The End date not available");}
+
+   console.log(this.chartPoints);
+    }
+  },
+};
+</script>
---
+++ app/pages/performance-page.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="c-dashboard">
+    <div class="c-filters__container">
+      <label for="Start" >Start date:</label>
+      <input type="date" id="Start" name="Start_data" v-model='startData' >
+      <label for="End">End date:</label>
+      <input type="date" id="End" name="End_data"  v-model='endData' >
+
+    </div>
+    <div class="c-dashboard__header">
+      <performance-chart-component :start="startData" :end="endData" />
+    </div>
+  </div>
+</template>
+
+<script>
+import PerformanceChartComponent from "../components/vue-components/performance-chart.vue";
+export default {
+  name: "AboutPageComponent",
+  components: {
+    PerformanceChartComponent,
+  },
+  data() {
+      return {
+        startData:"",
+        endData:"",
+      };
+  },
+  methods: {
+    view() {
+
+  },
+  handleChange () {
+
+    console.log(event.target);
+      const { name, value } = event.target;
+      //this.startData=value;
+      console.log(name,"**",value);
+      console.log(this.startData ,"--",this.endData);
+    },
+  handleChangeEnd () {
+        console.log("k",this.startData)
+        console.log(event.target);
+      const { name, value } = event.target;
+       this.endData=value;
+      console.log(name,value ,this.endData);
+
+
+  }
+}
+}
+</script>
